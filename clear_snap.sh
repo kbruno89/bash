71df0d14cb510c9f3e81491c780b3561841768e1
Bruno Kammers <bkammers@gmail.com>
2019-05-06 20:55:14 -0300

    Update clear_snap.sh

--- clear_snap.sh
+++ clear_snap.sh
@@ -11,11 +11,11 @@ for i in $NAME
 do
 	for j in $REV
 	do
-		echo -ne "\n	Removendo snap: $i  revision $j\n"
+		echo -ne "\n	Purge snap: $i  revision $j\n"
 		snap remove $i --revision=$j
 	done
 done
 
-echo -ne "\n\n Limpando journal:\n\n"
+echo -ne "\n\n Clean journal files:\n\n"
 journalctl --rotate && journalctl --vacuum-size=256M
 echo -ne "\n\n"
