6932bc6067af9473573bddecb68f23248a326040
kbruno89 <bkammers@gmail.com>
2019-05-06 20:35:52 -0300

    Add files via upload
    Clean old snaps versions on like Debian SO and clear journal files.

---
+++ clear_snap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NAME=`snap list --all | grep disabled | awk '{print $1}'`
+REV=`snap list --all | grep disabled | awk '{print $3}'`
+
+clear
+
+for i in $NAME
+do
+	for j in $REV
+	do
+		echo -ne "\n	Removendo snap: $i  revision $j\n"
+		snap remove $i --revision=$j
+	done
+done
+
+echo -ne "\n\n Limpando journal:\n\n"
+journalctl --rotate && journalctl --vacuum-size=256M
+echo -ne "\n\n"
