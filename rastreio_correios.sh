af863e199d5fd6505bb336edb90ae05df1ab8ffe
Bruno Kammers Ribeiro <bkammers@gmail.com>
2021-04-26 09:09:05 -0300

    rastreio_correios - Clean Code
    Clean Code (From 29 lines to 13)

--- rastreio_correios.sh
+++ rastreio_correios.sh
@@ -2,28 +2,12 @@
 clear
 
 ## TRACKER CORREIOS SHIPS FROM TERMINAL
-
+# 26/04/2021 - Clean Code (From 29 lines to 13)
 
 URL="https://www.websro.com.br/detalhes.php?P_COD_UNI"
-which curl > /dev/null 2>&1
+CURL=$(which curl > /dev/null 2>&1 ; echo $?)
 
-if [ "$?" != "0" ]
-        then
-                echo ""
-                echo " CURL IS REQUIRED...  PLEASE INSTALL CURL PACKAGE!"
-                echo ""
-                exit
-fi
-
-if [ "$1" = "" ]
-        then
-                echo ""
-                echo "  PARAMETER NOT FOUND..."
-                echo ""
-                echo "  USAGE:  ./rastreio_correios.sh CODIGO_DE_RASTREIO"
-                echo ""
-                exit
-fi
-
+[[ "$?" -ne "0" ]] && echo " CURL IS REQUIRED...  PLEASE INSTALL CURL PACKAGE!" || exit 1
+[[ -z "$1" ]] && echo -ne "\n  PARAMETER NOT FOUND... \n\n USAGE:  ./rastreio_correios.sh CODIGO_DE_RASTREIO\n"
 
 curl "$URL=$1" 2> /dev/null | grep '<table' -A21 | tr '[:blank:]' ' ' | tail -n+12 | sed -e 's/<tr>//g' -e 's/<td>//g' -e s/"<td valign='top'>"//g -e 's/<br>/\n/g' -e 's/<label>//g' -e s/"<\/label><\/td>"//g -e 's/<strong>//g' -e s/"<\/strong>"//g -e s/"<\/td>"//g -e s/"<\/tr>"//g -e s/"                 \/"//g -e 's/^[ \t]*//'
