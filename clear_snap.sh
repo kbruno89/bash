e095046b943abdc6c4188438538e9d1067081bbd
Bruno Kammers Ribeiro <bkammers@gmail.com>
2021-05-07 10:02:15 -0300

    Update clear_snap.sh v0.2
    ## v0.2  EM 07/05/2021 - PRO Code :)

--- clear_snap.sh
+++ clear_snap.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-
-## REMOVE SNAP DUPLICATED (OLD VERSION) ENTRIES
-## CLEAN OLD JOURNAL FILES
-## THIS A LITTLE HELP WITH DISK SPACE SIZE
-
-NAME=`snap list --all | grep disabled | awk '{print $1}'`
-REV=`snap list --all | grep disabled | awk '{print $3}'`
-
 clear
+## REMOVE OLD DUPLICATED SNAP ENTRIES
+## CLEAN OLD JOURNAL FILES
+## THIS IS A LITTLE HELP WITH DISK SPACE SIZE  :)
+## v0.2  EM 07/05/2021 - PRO Code :)
 
-for i in $NAME
+DISABLED=$(snap list --all | grep disabled | awk '{print $1" "$3}')
+OLDIFS=$IFS ; IFS=$'\n'
+for i in $DISABLED
 do
-	for j in $REV
-	do
-		echo -ne "\n	Purge snap: $i  revision $j\n"
-		snap remove $i --revision=$j
-	done
+        NAME=$(echo $i | awk '{print $1}')
+        REV=$(echo $i | awk '{print $2}')
+        echo -ne "\n    Purge snap: $NAME  revision $REV\n"
+        snap remove $NAME --revision=$REV > /dev/null 2>&1
+        echo -ne "\nOK!" ; echo -ne "\n\n"
+
 done
+IFS=$OLDIFS
 
 echo -ne "\n\n Clean journal files:\n\n"
-
 journalctl --rotate && journalctl --vacuum-size=256M
-
 echo -ne "\n\n"
