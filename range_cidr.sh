d138384116fdb69dc1681b33a87873ff07620475
Bruno Kammers Ribeiro <bkammers@gmail.com>
2021-07-16 09:29:13 -0300

    Update range_cidr.sh
    Bug fixes and improvements.

--- range_cidr.sh
+++ range_cidr.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-
 ## CAPTURA RANGE CIDR DE DOMINIOS ESPECIFICADOS EM DOMAIN.TXT
-## CONSULTA NAS BASES  RADB.NET/CYMRU.COM  E SALVA EM TXT 
-## SEPARADO POR DOMINIO
+## CONSULTA NAS BASES  RADB.NET/CYMRU.COM
 ##
 ## SCRIPT CRIADO POR BRUNO KAMMERS RIBEIRO
-## v0.1  EM 31/03/2016
-## v0.2  EM 14/04/2016 - Adicionado uma segunda base para consulta, caso não encontre registros na primeira
-## v0.3  EM 28/06/2016 - Ajustado a visualização dos resultados
-## v0.4  EM 05/07/2020 - Validacao de binarios e ajuste no nome das variaveis
-## v0.5  EM 26/04/2021 - Clean Code - de 59 linhas para 33
 
 clear
-echo -ne "\n\n\n                       Please Wait...\n"
+echo -ne "\n\n\n                       Please Wait...\n\n"
 NSL=$(which nslookup > /dev/null 2>&1 ; echo $?)
 WHO=$(which whois > /dev/null 2>&1 ; echo $?)
-[[ -z domain.txt ]] && echo "    Please create file  domain.txt  with domains that you want see CIDR" || exit 1
-[[ "$NSL" -ne "0" ]] && echo "    Please install:  nslookup" || exit 1
-[[ "$WHO" -ne "0" ]] && echo "    Please install:  whois" || exit 1
+[[ ! -f domain.txt ]] && echo "    Please create file  domain.txt  with domains that you want query CIDR" && exit 1 || FILE=$(wc -l domain.txt | awk '{print $1}')
+[[ $FILE -eq 0 ]] && echo "    Please input at least one domain on domain.txt" && exit 1
+[[ $NSL -ne 0 ]] && echo "    Please install:  nslookup" && exit 1
+[[ $WHO -ne 0 ]] && echo "    Please install:  whois" && exit 1
 
-for i in $(cat domain.txt) 
+for i in $(cat domain.txt)
 do
         VAR1=$(nslookup $i | grep Address | awk '{print $2}' | tail -1)
         VAR2=$(whois $VAR1 | grep OriginAS | awk '{print $2}')
@@ -27,7 +21,5 @@ do
         whois -h whois.radb.net !g$VAR2 | tr ' ' '\n' | grep ^[0-9] | sort -un > $i.txt
         whois -h whois.radb.net !gAS$VAR3 | tr ' ' '\n' | grep ^[0-9] | sort -un >> $i.txt
         sort -un $i.txt -o $i.txt
+        echo -ne "\n $i \n" && cat $i.txt && echo -ne "\n"
 done
-
-echo -ne "\n\n    See the result:\n\n"
-for i in $(cat domain.txt) ; do echo $i && cat $i.txt ; done
