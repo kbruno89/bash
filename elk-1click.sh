b39e9c7d583f6cad2eb0e33da2b63be193cfce29
Bruno Kammers Ribeiro <bkammers@gmail.com>
2021-07-30 13:07:06 -0300

    Update elk-1click.sh
    Script ajustado e validado para o novo Debian 11.
    
    Está disponível para uso tanto no Debian 10, quanto no 11 (que será lançado oficialmente em 14/08/2021)
    
    Instale o ambiente ELK STACK com apenas 1 click!
    
    https://wiki.debian.org/DebianBullseye
    https://www.elastic.co/pt/elastic-stack

--- elk-1click.sh
+++ elk-1click.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 clear
 # REQUISITOS
-# DEBIAN 10  /  2G RAM
+# DEBIAN 10 OU 11  /  2G RAM
 #
 # BRUNO KAMMERS RIBEIRO  -  28/07/21
 
 echo -ne "\t VALIDANDO...\n"
 ROOT=$(id -u)
-VER=$(cat /etc/debian_version | grep 10 > /dev/null 2>&1)
+VER=$(cat /etc/debian_version | egrep '10|11' > /dev/null 2>&1 ; echo $?)
 GPG="https://artifacts.elastic.co/GPG-KEY-elasticsearch"
 IP=$(ip a | grep global | awk '{print $2}' | cut -d\/ -f1)
 MEM=$(free -m | grep Mem | awk '{print $2}')
 fnCLEAN(){ echo 3 > /proc/sys/vm/drop_caches ; }
 [[ $ROOT -ne 0 ]] && echo -ne "\n\n     PRECISA EXECUTAR COMO ROOT\n             SAINDO ...\n" && exit 1
 [[ $VER -ne 0 ]] && echo -ne "\n\n     SEU SISTEMA PRECISA SER:  DEBIAN >= 10\n             SAINDO ...\n" && exit 1
-[[ $MEM -lt 1950 ]] && echo -ne "\n\n     NECESSARIO NO MINIMO 2 GB DE RAM\n             SAINDO ...\n" && exit 1
+[[ $MEM -lt 1950 ]] && echo -ne "\n\n     NECESSARIO NO MINIMO 2G DE RAM\n             SAINDO ...\n" && exit 1
 export DEBIAN_FRONTEND=noninteractive
 echo -ne "\t     OK!\n\n"
 
@@ -39,9 +39,9 @@ sed -ri 's/#network.host: 192.168.0.1/network.host: '$IP'/g' /etc/elasticsearch/
 sed -ri 's/#http.port/http.port/g' /etc/elasticsearch/elasticsearch.yml
 echo "discovery.seed_hosts: []" >> /etc/elasticsearch/elasticsearch.yml
 echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
-# ESTA SENDO ATRIBUIDO 512M DE RAM PARA ALOCACAO DO JAVA
+# ESTA SENDO ATRIBUIDO 256M DE RAM PARA ALOCACAO DO JAVA NESTE SERVICO
 # VOCE PODE AUMENTAR ISTO DE ACORDO COM OS RECURSOS DISPONIVEIS EM SEU SERVIDOR
-sed -ri 's/## -Xms4g/-Xms512m/g' /etc/elasticsearch/jvm.options
+sed -ri 's/## -Xms4g/-Xms256m/g' /etc/elasticsearch/jvm.options
 sed -ri 's/## -Xmx4g/-Xmx512m/g' /etc/elasticsearch/jvm.options
 systemctl enable --now elasticsearch > /dev/null 2>&1
 RUN=$(systemctl --type=service --state=active | grep elasticsearch > /dev/null ; echo $?)
@@ -61,6 +61,12 @@ RUN=$(systemctl --type=service --state=active | grep kibana > /dev/null ; echo $
 
 echo -ne "\n\t INSTALANDO LOGSTASH..."
 fnCLEAN && apt-get install logstash -qq > /dev/null
+
+echo -ne "\n\t CONFIGURANDO LOGSTASH...\n"
+# ESTA SENDO ATRIBUIDO 256M DE RAM PARA ALOCACAO DO JAVA NESTE SERVICO
+# VOCE PODE AUMENTAR ISTO DE ACORDO COM OS RECURSOS DISPONIVEIS EM SEU SERVIDOR
+sed -ri 's/-Xms1g/-Xms256m/g' /etc/logstash/jvm.options
+sed -ri 's/-Xmx1g/-Xmx512m/g' /etc/logstash/jvm.options
 systemctl enable --now logstash > /dev/null 2>&1
 RUN=$(systemctl --type=service --state=active | grep logstash > /dev/null ; echo $?)
 [[ $RUN -eq 0 ]] && echo -ne "\t     OK!\n"
